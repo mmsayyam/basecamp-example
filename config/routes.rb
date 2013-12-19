f53d6dd6e15df4b8f4582cac401ff255d372fffa
Sayyam <sayyam@sweetpixelstudios.com>
2013-12-19 22:46:34 +0500

    Signup, Login, Logout working properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BasecampExample::Application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'signin', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   resources :users
   resources :sessions
 
