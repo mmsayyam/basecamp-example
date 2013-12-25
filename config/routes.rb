a612a09fec4dfad52cf29c6fb2b179f1082995d6
aliirz <dejavu87@gmail.com>
2013-12-25 13:49:31 +0500

    live reload wod working with grunt

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ BasecampExample::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#welcome'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
